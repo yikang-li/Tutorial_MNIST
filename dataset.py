c39d0958f3514d1722c407907fe2cc98b06e41ed
Yikang <allen.li.thu@gmail.com>
2019-04-24 12:04:08 +0800

    first commit

---
+++ dataset.py
@@ -0,0 +1,20 @@
+from torch.utils.data import Dataset
+
+class myMNISTDataset(Dataset):
+    """Face Landmarks dataset."""
+
+    def __init__(self, root_dir, transform=None):
+        """
+        Args:
+            root_dir (string): Directory with all the folders of images.
+            transform (callable, optional): Optional transform to be applied
+                on a sample.
+        """
+        self.root_dir = root_dir
+        self.transform = transform
+
+    def __len__(self):
+        return num_images
+
+    def __getitem__(self, idx):
+        return (data, target)
